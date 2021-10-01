0f3f3b1b6f65827a22f6e871c813af05bae7c802
Sasha <sash-2007@ya.ru>
2021-10-01 16:30:03 +0500

    tracker all -5

--- src/main/java/ru/job4j/tracker/store/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/store/SqlTracker.java
@@ -13,6 +13,13 @@ public class SqlTracker implements Store {
 
     private Connection cn;
 
+    public SqlTracker() {
+    }
+
+    public SqlTracker(Connection cn) {
+        this.cn = cn;
+    }
+
     public void init() {
         try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
             Properties config = new Properties();
